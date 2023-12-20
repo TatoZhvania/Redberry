7e5081b6a49b203b4ee9389443569dc1015ed790
TatoZhvania <tatojvania.03@gmail.com>
2023-12-20 23:30:47 +0400

    Splitting into two components and adding localstorage

---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,90 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Modal from 'react-modal';
+import addBtn from '../assets/add.svg';
+import tick from '../assets/tick-circle.svg';
+import infoCircle from '../assets/info-circle.svg';
+
+const CustomModal = ({
+  isOpen,
+  onRequestClose,
+  isAuthenticated,
+  closeModal,
+  email,
+  setEmail,
+  emailError,
+  handleLogin,
+}) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Login Modal"
+      className="w-[480px] h-80 rounded-xl bg-[#FFF] text-[#1A1A1F] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-[#1A1A1F] bg-opacity-50"
+    >
+      {isAuthenticated ? (
+        <>
+          <button className="absolute right-5 top-5" onClick={closeModal}>
+            <img src={addBtn} alt="add-btn" />
+          </button>
+
+          <div className="mx-10 my-10 mt-16">
+            <div className="flex flex-col gap-4 text-center items-center">
+              <img src={tick} alt="tick" className="w-16 h-16" />
+              <p className="text-[#1A1A1F] text-xl font-bold">
+                წარმატებული ავტორიზაცია
+              </p>
+              <button
+                className="bg-[#5D37F3] text-sm w-full py-3 rounded-lg mt-6 text-[#fff]"
+                onClick={closeModal}
+              >
+                კარგი
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <button className="absolute right-5 top-5" onClick={closeModal}>
+            <img src={addBtn} alt="add-btn" />
+          </button>
+
+          <div className="mx-10 my-10 mt-16">
+            <p className="text-[#1A1A1F] text-2xl font-bold text-center mb-6">
+              შესვლა
+            </p>
+            <div className="flex flex-col gap-4">
+              <p className="text-[#1A1A1F] text-sm font-medium">ელ-ფოსტა</p>
+              <input
+                placeholder="Example@redberry.ge"
+                type="text"
+                className={`rounded-xl bg-[#F7F7FF] h-11 text-[#85858D] text-sm border-[1.5px] ${
+                  emailError ? 'border-red-500' : 'border-[#5D37F3]'
+                } outline-none p-4`}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {emailError && (
+                <p className="text-[#EA1919] text-sm font-normal flex items-center gap-2">
+                  <img src={infoCircle} alt="info-circle" />
+                  {emailError}
+                </p>
+              )}
+            </div>
+
+            <button
+              className="bg-[#5D37F3] w-full py-3 rounded-lg mt-6 text-[#fff] text-sm "
+              onClick={handleLogin}
+            >
+              შესვლა
+            </button>
+          </div>
+        </>
+      )}
+    </Modal>
+  );
+};
+
+export default CustomModal;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/LOGO-02 3.svg';
-import addBtn from '../assets/add.svg';
-import tick from '../assets/tick-circle.svg';
-import infoCircle from '../assets/info-circle.svg';
+import CustomModal from './CustomModal';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    const storedIsAuthenticated = localStorage.getItem('isAuthenticated');
+
+    if (storedEmail && storedIsAuthenticated) {
+      setEmail(storedEmail);
+      setIsAuthenticated(storedIsAuthenticated === 'true');
+    }
+  }, []);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -40,9 +48,7 @@ const Header = () => {
     })
       .then((response) => {
         // console.log('Response status:', response.status);
-        if (response.status === 200) {
-          return response.json();
-        } else if (response.status === 204) {
+        if (response.status === 204) {
           setIsAuthenticated(true);
           setEmailError('');
           return {};
@@ -74,12 +80,14 @@ const Header = () => {
         <div>
           {isAuthenticated ? (
             <>
-              <button
-                className="bg-[#5D37F3] rounded-lg text-white px-4 py-3 text-sm"
-                onClick={openModal}
-              >
-                დაამატე ბლოგი
-              </button>
+              <Link to="modal-form">
+                <button
+                  className="bg-[#5D37F3] rounded-lg text-white px-4 py-3 text-sm"
+                  onClick={openModal}
+                >
+                  დაამატე ბლოგი
+                </button>
+              </Link>
             </>
           ) : (
             <>
@@ -95,75 +103,16 @@ const Header = () => {
       </div>
 
       <div className="absolute top-1">
-        <Modal
+        <CustomModal
           isOpen={isModalOpen}
           onRequestClose={closeModal}
-          contentLabel="Login Modal"
-          className="w-[480px] h-80 rounded-xl bg-[#FFF] text-[#1A1A1F]
-            absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  
-          "
-          overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-[#1A1A1F] bg-opacity-50"
-        >
-          {isAuthenticated ? (
-            <>
-              <button className="absolute right-5 top-5" onClick={closeModal}>
-                <img src={addBtn} alt="add-btn" />
-              </button>
-
-              <div className="mx-10 my-10 mt-16">
-                <div className="flex flex-col gap-4 text-center items-center">
-                  <img src={tick} alt="tick" className="w-16 h-16" />
-                  <p className="text-[#1A1A1F] text-xl font-bold">
-                    წარმატებული ავტორიზაცია
-                  </p>
-                  <button
-                    className="bg-[#5D37F3] text-sm w-full py-3 rounded-lg mt-6 text-[#fff]"
-                    onClick={closeModal}
-                  >
-                    კარგი
-                  </button>
-                </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <button className="absolute right-5 top-5" onClick={closeModal}>
-                <img src={addBtn} alt="add-btn" />
-              </button>
-
-              <div className="mx-10 my-10 mt-16">
-                <p className="text-[#1A1A1F] text-2xl font-bold text-center mb-6">
-                  შესვლა
-                </p>
-                <div className="flex flex-col gap-4">
-                  <p className="text-[#1A1A1F] text-sm font-medium">ელ-ფოსტა</p>
-                  <input
-                    placeholder="Example@redberry.ge"
-                    type="text"
-                    className={`rounded-xl bg-[#F7F7FF] h-11 text-[#85858D] text-sm border-[1.5px] ${
-                      emailError ? 'border-red-500' : 'border-[#5D37F3]'
-                    } outline-none p-4`}
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  {emailError && (
-                    <p className="text-[#EA1919] text-sm font-normal flex items-center gap-2">
-                      <img src={infoCircle} alt="info-circle" />
-                      {emailError}
-                    </p>
-                  )}
-                </div>
-
-                <button
-                  className="bg-[#5D37F3] w-full py-3 rounded-lg mt-6 text-[#fff] text-sm "
-                  onClick={handleLogin}
-                >
-                  შესვლა
-                </button>
-              </div>
-            </>
-          )}
-        </Modal>
+          isAuthenticated={isAuthenticated}
+          closeModal={closeModal}
+          email={email}
+          setEmail={setEmail}
+          emailError={emailError}
+          handleLogin={handleLogin}
+        />
       </div>
     </>
   );
